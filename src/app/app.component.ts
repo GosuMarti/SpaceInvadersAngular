236940764a5e83385f789718a9c3c375eb08865c
Martin Venev <martiveselinov@gmail.com>
2025-01-06 10:38:05 +0100

    add imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from "../components/navbar/navbar.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent], // Import RouterModule
+  imports: [CommonModule, RouterModule, NavbarComponent],
   template: `
     <app-navbar></app-navbar>
     <router-outlet></router-outlet>
