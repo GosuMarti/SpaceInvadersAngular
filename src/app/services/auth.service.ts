b66fe72501ec75f4f80aed317ff72951d6501f00
Martin Venev <martiveselinov@gmail.com>
2025-01-06 13:15:29 +0100

    add auto log out if token expires

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -7,11 +8,36 @@ import { BehaviorSubject } from 'rxjs';
 export class AuthService {
   private loggedIn = new BehaviorSubject<boolean>(!!localStorage.getItem('token'));
 
+  constructor(private router: Router) {}
+
   get isLoggedIn$() {
     return this.loggedIn.asObservable();
   }
 
   setLoginStatus(isLoggedIn: boolean): void {
     this.loggedIn.next(isLoggedIn);
+  }
+
+  autoLogout() {
+    const expirationTime = localStorage.getItem('tokenExpiration');
+    if (!expirationTime) return;
+
+    const timeout = parseInt(expirationTime, 10) - new Date().getTime();
+    if (timeout > 0) {
+      setTimeout(() => {
+        this.logout();
+      }, timeout);
+    } else {
+      this.logout();
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('tokenExpiration');
+    localStorage.removeItem('username');
+    this.setLoginStatus(false);
+    this.router.navigateByUrl('/login');
+    alert('Session expired. Please log in again.');
   }
 }
