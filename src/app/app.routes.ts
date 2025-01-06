af4ab6d53c2fc4a446a558db0ff5df4271c71d08
Martin Venev <martiveselinov@gmail.com>
2025-01-06 10:36:37 +0100

    add records path and imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../components/home/home.component';
-import { PlayComponent } from '../components/play/play.component';
-import { LoginComponent } from '../components/login/login.component';
-import { PreferencesComponent } from '../components/preferences/preferences.component';
-import { RegisterComponent } from '../components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { PlayComponent } from './components/play/play.component';
+import { LoginComponent } from './components/login/login.component';
+import { PreferencesComponent } from './components/preferences/preferences.component';
+import { RegisterComponent } from './components/register/register.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { RecordsComponent } from './components/records/records.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'play', component: PlayComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'preferences', component: PreferencesComponent }
+  { path: 'records', component: RecordsComponent },
+  { path: 'preferences', component: PreferencesComponent },
+  { path: '**', redirectTo: 'register', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [provideHttpClient(withFetch())]
 })
 export class AppRoutingModule { }
