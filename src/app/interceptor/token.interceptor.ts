1ab3687f6f0a2615973f24089cc7a31c42a9d729
Martin Venev <martiveselinov@gmail.com>
2025-01-06 13:16:06 +0100

    add token expiration check

--- src/app/interceptor/token.interceptor.ts
+++ src/app/interceptor/token.interceptor.ts
@@ -2,10 +2,25 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem("token");
-  const newRequest = req.clone({
-    setHeaders:{
-      Authorization:`${token}`
+  const expirationTime = localStorage.getItem("tokenExpiration");
+
+  if (token && expirationTime) {
+    const currentTime = new Date().getTime();
+    
+    // Check if the token is expired
+    if (currentTime > parseInt(expirationTime, 10)) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("tokenExpiration");
+      alert('Session expired. Please log in again.');
+      return next(req);
     }
-  })
+  }
+
+  const newRequest = req.clone({
+    setHeaders: {
+      Authorization: `${token}`
+    }
+  });
+
   return next(newRequest);
 };
