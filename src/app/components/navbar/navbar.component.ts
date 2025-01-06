c2eb3b8b10d0912629ad6bc84dd4d826e52b106b
Martin Venev <martiveselinov@gmail.com>
2025-01-06 11:11:43 +0100

    add logout option if logged in

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, RouterOutlet],
+  imports: [RouterModule, RouterOutlet, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn = false;
 
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe(status => {
+      this.isLoggedIn = status;
+    });
+  }
+
+  onLogout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    this.authService.setLoginStatus(false);
+    alert('You have been logged out.');
+    this.router.navigateByUrl('/');
+  }
 }
