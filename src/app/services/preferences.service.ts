0ded8d3c65c26581c4a385840a88d30cddac6fa1
Martin Venev <martiveselinov@gmail.com>
2025-01-08 19:15:26 +0100

    add preferences logic

--- src/app/services/preferences.service.ts
+++ src/app/services/preferences.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreferencesService {
+  private baseUrl = 'http://localhost:3000/preferences';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPreferences(username: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${username}`);
+  }
+
+  updatePreferences(username: string, ufos: number, time: number): Observable<any> {
+    return this.http.post(this.baseUrl, { username, ufos, time });
+  }
 }
